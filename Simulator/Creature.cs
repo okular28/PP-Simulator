454a745d92bb08d549a31178599befd7a8d15693
okular28 <pawel.siola2003@gmail.com>
2024-12-13 20:12:53 +0100

    Stwory

---
+++ Simulator/Creature.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator;
+internal class Creature
+{
+    public string Name { get; } = "NoName";
+    public int Level { get; set; } = 1;
+    public Creature(string name, int level)
+    {
+        Name = name;
+        Level = level;
+    }
+    public Creature()
+    {
+    }
+    public string Info => $"{Name} [{Level}]";
+    public void SayHi() => Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+}
--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -1,8 +1,17 @@
-﻿namespace Simulator;
+﻿using System.ComponentModel;
+
+namespace Simulator;
     internal class Program
     {
     static void Main(string[] args)
     {
         Console.WriteLine("Starting simulator!\n");
+        var stwor = new Creature();
+        stwor.SayHi();
+        stwor.Level = 16;
+        stwor.SayHi();
+        Console.WriteLine(stwor.Info);
+        Animals kot = new() { Description = "kot", Size = 16 };
+        Console.WriteLine(kot.Info);
     }
 }
